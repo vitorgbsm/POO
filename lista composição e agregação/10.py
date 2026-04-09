439007395777c180339fb3f9d5bb8bea3d428552
vitor <vitor.sales@escolar.ifrn.edu.br>
2026-04-09 10:05:42 -0300

    .

--- lista composição e agregação/10.py
+++ lista composição e agregação/10.py
@@ -1,22 +1,59 @@
+class Aluno:
+    def __init__(self, aluno):
+        self.aluno = aluno
+
+    def __str__(self):
+        return f"Aluno: {self.aluno}"
+    
 class Turma:
-    def __init__(self, nome, turma):
-        self.nome = nome
-        self.turma = []
+    def __init__(self, turma):
+        self.turma = turma
+        self.alunos = []
+    
+    def __str__(self):
+        return f"{self.turma}"
 
     def adicionar_aluno(self, aluno):
-        self.turma
+        self.alunos.append(aluno)
 
-    def __str__(self):
-        return f"Turma: {self.turma}"
-
-class Aluno:
-    def __init__(self, aluno, turma):
-        self.aluno = aluno
-        self.turma = turma
-
-    def __str__(self):
-        return f"Aluno(a): {self.aluno}"
-
+    def exibir_turma(self):
+        print(f"{self.turma}:")
+        for alunos in self.alunos:
+            print(f"{alunos}")
+    
 class Escola:
     def __init__(self, nome):
-        pass
+        self.nome = nome
+        self.turmas = []
+    
+    def adicionar_turma(self, turma):
+        self.turmas.append(turma)
+    
+    def exibir_turmas(self):
+        print(f"Turmas do {self.nome}")
+        for turmas in self.turmas:
+            turmas.exibir_turma()
+
+a1 = Aluno("Vitor Gabriel")
+a2 = Aluno("Helia Rafaela")
+a3 = Aluno("Arthur Vinicius")
+a4 = Aluno("Bernardo Miranda")
+a5 = Aluno("Luiz Augusto")
+a6 = Aluno("Andrielly Rodrigues")
+
+t1 = Turma("INFO2V")
+t2 = Turma("INFO2VA")
+
+ifrn = Escola("IFRN")
+
+t1.adicionar_aluno(a1)
+t1.adicionar_aluno(a2)
+t1.adicionar_aluno(a3)
+t2.adicionar_aluno(a4)
+t2.adicionar_aluno(a5)
+t2.adicionar_aluno(a6)
+
+ifrn.adicionar_turma(t1)
+ifrn.adicionar_turma(t2)
+
+ifrn.exibir_turmas()
