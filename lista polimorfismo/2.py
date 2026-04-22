ff87b0f4389718d474a904e694bea2d153be7c05
vitor <vitor.sales@escolar.ifrn.edu.br>
2026-04-22 13:59:49 -0300

    .

---
+++ lista polimorfismo/2.py
@@ -0,0 +1,12 @@
+class Animal:
+    def emitir_som(self):
+        print("Algum som generico")
+
+class Cachorro:
+    def emitir_som(self):
+        print("Latido")
+
+class Gato:
+    def emitir_som(self):
+        print("Miado")
+
